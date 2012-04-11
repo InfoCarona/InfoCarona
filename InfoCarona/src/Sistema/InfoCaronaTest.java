537ef61ab0d75e1233d67a52c50c97d444d80729
Felipeefb <malkavianbr@gmail.com>
2012-04-11 10:36:09 -0300

    Agora com mais 6 USerstories bugados para a nossa alegria =D

--- InfoCarona/src/Sistema/InfoCaronaTest.java
+++ InfoCarona/src/Sistema/InfoCaronaTest.java
@@ -19,8 +19,14 @@ public class InfoCaronaTest {
         //files.add("US01.txt");
         //files.add("US02.txt");
         //files.add("US03.txt");
-        files.add("US04.txt");
-        //files.add("US05.txt");
+//        files.add("US04.txt");
+//        files.add("US05.txt");
+//		  files.add("US06.txt");
+//		  files.add("US07.txt");
+//		  files.add("US08.txt");
+//		  files.add("US09.txt");
+//		  files.add("US11.txt");
+//		  files.add("US12.txt");
         //Instantiate the sistena
         
         //Instantiate EasyAccept façade
